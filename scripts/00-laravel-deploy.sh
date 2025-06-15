a82e518f30024889d7a75d4b2d6ae3897a9c0970
official-kalyani <83694769+official-kalyani@users.noreply.github.com>
2025-06-15 11:13:35 +0530

    updated script
    updated script

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-echo "Running composer"
-composer global require hirak/prestissimo
-composer install --no-dev --working-dir=/var/www/html
 
-echo "generating application key..."
-php artisan key:generate --show
+echo "Installing dependencies..."
+composer install --no-dev --optimize-autoloader
+
+echo "Generating application key..."
+php artisan key:generate --force
 
 echo "Caching config..."
 php artisan config:cache
